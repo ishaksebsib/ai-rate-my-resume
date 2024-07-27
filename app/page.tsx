3fa74a145151020da007dcb345dff49e517ae298
ishak <ishaksebsib@gmail.com>
2024-07-27 17:28:18 +0300

    ENH: Add reponsiveness to hero page

--- app/page.tsx
+++ app/page.tsx
@@ -4,21 +4,21 @@ export default function Home() {
   return (
     <main className="w-screen flex justify-center h-[100vh] bg-background bg-[linear-gradient(to_bottom,#000,#200D42_34%,#4F21A1_65%,#A46EDB_98%)]">
       {/* hero section */}
-      <section className="w-full p-8 flex items-center justify-between h-[95vh] max-w-[90%] bg-dnark">
+      <section className="w-full flex items-center justify-between h-[100vh]  bg-dnark p-2 sm:max-w-[90%] md:p-8">
         {/* text container */}
-        <div className="text-foreground w-full flex flex-col gap-7">
-          <div className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-7xl">
+        <div className="text-foreground w-full flex flex-col items-center gap-4 sm:gap-7 xl:items-start">
+          <div className="scroll-m-20  font-extrabold tracking-tight text-4xl text-center sm:text-5xl md:text-6xl lg:text-7xl lg:w-full xl:text-start">
             <h1>Transform Your Resume</h1>
-            <h1 className="pt-2">Transform Your Caree</h1>
+            <h1 className="pt-2">Transform Your Career</h1>
           </div>
-          <p className="text-2xl pl-4 w-3/4 text-darkwhite font-light">
+          <p className="text-darkwhite font-light p-4 text-sm w-full text-center sm:text-xl sm:pl-4 md:text-2xl lg:max-w-3xl xl:text-start">
             Get expert feedback and tailored recommendations to elevate your
             resume and land your dream job.
           </p>
 
           {/* file upload button container */}
-          <div className="w-2/4 h-1/4 pl-4">
-            <label className="flex flex-col items-center justify-center w-3/4 h-44 border-2 border-dashed rounded-lg cursor-pointer">
+          <div className="w-full h-full  md:w-3/4 md:h-1/4 md:pl-4">
+            <label className="flex flex-col items-center justify-center h-44 w-full xl:w-3/4  border-2 border-dashed rounded-lg cursor-pointer">
               <div className="flex flex-col items-center justify-center pt-5 pb-6">
                 <svg
                   className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
@@ -37,8 +37,8 @@ export default function Home() {
                   />{" "}
                 </svg>
                 <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                  <span className="font-semibold">Click to upload</span> or drag
-                  and drop
+                  <span className="font-semibold">Click to upload</span>
+                  <span className="hidden sm:block">or drag and drop</span>{" "}
                 </p>
                 <p className="text-xs text-gray-500 dark:text-gray-400">
                   SVG, PNG, JPG or GIF (MAX. 800x400px)
@@ -50,7 +50,7 @@ export default function Home() {
         </div>
 
         {/* image container */}
-        <div className="w-auto flex justify-center items-center ">
+        <div className="hidden xl:flex xl:justify-center xl:items-center xl:w-[35%]">
           <Image
             src={resumeIlustration}
             alt="reusme ilustration"
