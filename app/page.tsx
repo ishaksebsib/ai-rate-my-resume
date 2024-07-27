23dadc292eafa335618fff34a4614843103cd711
ishak <ishaksebsib@gmail.com>
2024-07-27 18:02:13 +0300

    ENH: Add notification

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import { Toaster } from "react-hot-toast";
 import HeroContainer from "./_components/HeroContainer";
 
 export default function Home() {
 	return (
 		<main className="w-screen flex justify-center h-[100vh] bg-background bg-[linear-gradient(to_bottom,#000,#200D42_34%,#4F21A1_65%,#A46EDB_98%)]">
+			{/* toast notification */}
+			<Toaster />
 			{/* hero section */}
 			<HeroContainer />
 		</main>
