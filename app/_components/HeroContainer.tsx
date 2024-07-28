a3f548dbf1f012c8220110561ae29e29ed7e85f4
ishak <ishaksebsib@gmail.com>
2024-07-28 11:56:03 +0300

    [ENH]: Improve Error hndling

--- app/_components/HeroContainer.tsx
+++ app/_components/HeroContainer.tsx
@@ -5,6 +5,7 @@ import axios from "axios";
 import { CONSTANT } from "@/constant";
 import { useRouter } from "next/navigation";
 import toast from "react-hot-toast";
+import { handleAxiosError } from "@/lib/utils";
 
 export enum UploadStatus {
   INITIAL = "INITIAL",
@@ -24,19 +25,30 @@ const HeroContainer = () => {
     formData.append("file", file);
 
     // Validate file type
-    const validFileTypes = [
-      "application/pdf",
-      "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-    ];
-    if (!validFileTypes.includes(file.type)) {
-      toast.error("Invalid file type. Please upload a PDF or DOCX file.");
-      return;
+    {
+      /*
+       *const validFileTypes = [
+       *  "application/pdf",
+       *  "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+       *];
+       *if (!validFileTypes.includes(file.type)) {
+       *  toast.error("Invalid file type. Please upload a PDF or DOCX file.");
+       *  return;
+       *}
+       */
     }
 
     // Upload file
+    const uploadTost = toast.loading("Uploading file...", { duration: 3000 });
+    let uploadError = false;
+    setUploadStatus(UploadStatus.UPLOADING);
     try {
-      //console.log("uploading file");
-      setUploadStatus(UploadStatus.UPLOADING);
+      setTimeout(() => {
+        if (!uploadError) {
+          toast.loading("Analayzing resume...", { id: uploadTost });
+        }
+      }, 3000);
+
       const response = await axios.post(CONSTANT.URLS.uploadResume, formData, {
         headers: {
           "Content-Type": "multipart/form-data",
@@ -45,10 +57,20 @@ const HeroContainer = () => {
 
       //console.log("File uploaded successfully:", response.data);
       setUploadStatus(UploadStatus.UPLOADED);
+      toast.success("Result is ready!", { id: uploadTost });
+
       router.push("/result/" + response.data.result_id);
+      return;
     } catch (error) {
       //console.error("Error uploading file:", error);
+      uploadError = true;
+      const errorMessage = handleAxiosError(
+        error,
+        "Something went wrong. Please try again.",
+      );
+      toast.error(errorMessage, { id: uploadTost });
       setUploadStatus(UploadStatus.ERROR);
+      return;
     }
   };
 
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,26 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { AxiosError } from "axios";
+import { type ClassValue, clsx } from "clsx";
+import toast from "react-hot-toast";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const handleAxiosError = (
+  error: AxiosError | any,
+  fallBackMessage: string,
+): string => {
+  let message = fallBackMessage;
+
+  if (error.response) {
+    message = error.response.data.detail;
+    if (message) {
+      return message;
+    }
+  } else if (error.request) {
+    message = "No response received from server";
+  }
+
+  return message;
+};
